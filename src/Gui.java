9953ffca10d4ca16ffa903cf2cab7f96e6595776
debonatis <deb@bamvpn.net>
2012-03-14 13:48:29 +0100

    gone

--- src/Gui.java
+++ src/Gui.java
@@ -1,14 +1,1 @@
-import javax.swing.*;
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Rino
- */
-public class Gui extends JFrame {
-    private Brett brett;
-    //dmd      
-}
