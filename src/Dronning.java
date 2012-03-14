326d596d286b1d55dac8b6bd857ea40b8ad9a43a
debonatis <deb@bamvpn.net>
2012-03-14 13:55:28 +0100

    gg

--- src/Dronning.java
+++ src/Dronning.java
@@ -16,33 +16,35 @@ public class Dronning extends Brikke {
     public ArrayList<Rute> sjekkLovligeTrekk() {
     ArrayList<Rute> lovligeRuter = new ArrayList<>();
         Rute current = super.getCurrent();
+        int x = current.getX();
+        int y = current.getY();
         
-        for(int i = current.getX()+1; i < 9; i++){
+        for(int i = x+1; i < 9; i++){
                 lovligeRuter.add(new Rute(i,current.getY()));
             }       
-        for(int i = current.getY() +1; i < 9; i--){
+        for(int i = y +1; i < 9; i--){
               lovligeRuter.add(new Rute(current.getX(),i));
            
         }
-        for(int i = current.getX() -1; i > 0; i--){
+        for(int i = x -1; i > 0; i--){
                lovligeRuter.add(new Rute(i,current.getY()));
             
         }
-        for(int u = current.getY() - 1; u > 0; u--){
+        for(int u = y - 1; u > 0; u--){
                 lovligeRuter.add(new Rute(current.getX(),u));
             }
-        for(int i = current.getX() -1; i > 0; i--){
-            for(int u = current.getY() - 1; i>0; i--){
+        for(int i = x -1; i > 0; i--){
+            for(int u = y - 1; i>0; i--){
                 lovligeRuter.add(new Rute(i,u));
             }//lol
         }
-        for(int i = current.getX() -1; i > 0; i--){
-            for(int u = current.getY() + 1; i < 8; i++){
+        for(int i = x -1; i > 0; i--){
+            for(int u = y + 1; i < 8; i++){
                 lovligeRuter.add(new Rute(i,u));
             }
         }
-        for(int i = current.getX() + 1; i < 8; i++){
-            for(int u = current.getY() - 1; u > 0; u--){
+        for(int i = x + 1; i < 8; i++){
+            for(int u = y - 1; u > 0; u--){
                 lovligeRuter.add(new Rute(i,u));
             }
         }
@@ -50,5 +52,3 @@ public class Dronning extends Brikke {
         return lovligeRuter;
     }
 }
-    
-
