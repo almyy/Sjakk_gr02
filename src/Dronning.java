00831b54fbc0c06c6a4e4112d61ac80b9069c711
debonatis <deb@bamvpn.net>
2012-03-14 13:45:36 +0100

    simon

--- src/Dronning.java
+++ src/Dronning.java
@@ -7,6 +7,48 @@
  *
  * @author Rino
  */
-public class Dronning {
+public class Dronning extends Brikke {
     
+    public Dronning(Rute start) {
+            super(start,6);
+    
+} 
+    public ArrayList<Rute> sjekkLovligeTrekk() {
+    ArrayList<Rute> lovligeRuter = new ArrayList<>();
+        Rute current = super.getCurrent();
+        
+        for(int i = current.getX()+1; i < 9; i++){
+                lovligeRuter.add(new Rute(i,current.getY()));
+            }       
+        for(int i = current.getY() +1; i < 9; i--){
+              lovligeRuter.add(new Rute(current.getX(),i));
+           
+        }
+        for(int i = current.getX() -1; i > 0; i--){
+               lovligeRuter.add(new Rute(i,current.getY()));
+            
+        }
+        for(int u = current.getY() - 1; u > 0; u--){
+                lovligeRuter.add(new Rute(current.getX(),u));
+            }
+        for(int i = current.getX() -1; i > 0; i--){
+            for(int u = current.getY() - 1; i>0; i--){
+                lovligeRuter.add(new Rute(i,u));
+            }//lol
+        }
+        for(int i = current.getX() -1; i > 0; i--){
+            for(int u = current.getY() + 1; i < 8; i++){
+                lovligeRuter.add(new Rute(i,u));
+            }
+        }
+        for(int i = current.getX() + 1; i < 8; i++){
+            for(int u = current.getY() - 1; u > 0; u--){
+                lovligeRuter.add(new Rute(i,u));
+            }
+        }
+        
+        return lovligeRuter;
+    }
 }
+    
+
