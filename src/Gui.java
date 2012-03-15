90f2ed6280e78a0f62a2c552d76c9c121f8ff8f0
Martin <Martin@158.38.121.143>
2012-03-15 12:34:39 +0100

    martinB

--- src/Gui.java
+++ src/Gui.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.net.*;
@@ -14,54 +15,39 @@ import javax.swing.*;
 class GUI extends JFrame{
 
     private Brett brett;
-    JFrame frame;
-    JPanel squares[][] = new JPanel[8][8];
     //dmd
 
-    public GUI(String tittel) {
+    
+    public JFrame frame;
+    public JPanel squares[][] = new JPanel[8][8];
 
-        frame = new JFrame(tittel);
-        setBounds(400,10,800,800);
-        frame.setSize(500, 500);
+    public GUI() {
+        frame = new JFrame("Gr.2 GUItest");
+        frame.setSize(500, 400);
         frame.setLayout(new GridLayout(8, 8));
+        
 
+        
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 squares[i][j] = new JPanel();
 
                 if ((i + j) % 2 == 0) {
-                    squares[i][j].setBackground(Color.black);
-                } else {
                     squares[i][j].setBackground(Color.white);
+                } else {
+                    squares[i][j].setBackground(Color.black);
                 }
                 frame.add(squares[i][j]);
             }
         }
         
-        
-        squares[0][0].add(new JLabel(new ImageIcon("C:/Users/Martin/Desktop/taarn.png")));//Tårn
-        squares[0][2].add(new JLabel(new ImageIcon("")));//løper
-
-        squares[0][4].add(new JLabel(new ImageIcon("")));//hest
-        squares[0][5].add(new JLabel(new ImageIcon("")));//løper
-        squares[0][7].add(new JLabel(new ImageIcon("")));//Taarn
-
-        squares[7][0].add(new JLabel(new ImageIcon("")));
-        squares[7][2].add(new JLabel(new ImageIcon("")));
-        squares[7][4].add(new JLabel(new ImageIcon("")));
-        squares[7][5].add(new JLabel(new ImageIcon("")));
-        squares[7][7].add(new JLabel(new ImageIcon("")));
-
-        for (int i = 0; i < 8; i++) {
-            squares[1][i].add(new JLabel(new ImageIcon("")));
-            squares[6][i].add(new JLabel(new ImageIcon("")));
-        }
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.setVisible(true);
     }
+
     public static void main(String[] args) {
-        GUI sjakk = new GUI("Sjakk");
-        sjakk.setVisible(true);
+        
+        GUI b = new GUI();
+        
     }
 }
