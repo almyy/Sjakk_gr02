b4bd53144b843252ed192758c480681366b212c7
Rino <Rino@Solstad>
2012-03-15 13:05:56 +0100

    gui

--- src/Gui.java
+++ src/Gui.java
@@ -1,62 +1,40 @@
 
-import javax.swing.*;
-import java.awt.*;
-import java.net.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 class Gui extends JFrame {
-
-    private Brett brett;
-    //dmd
-
-    
-    public JFrame frame;
-    public JPanel squares[][] = new JPanel[8][8];
-
-    public GUI() {
-        frame = new JFrame("Gr.2 GUItest");
-        frame.setSize(500, 400);
-        frame.setLayout(new GridLayout(8, 8));
-        
-
-        
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                squares[i][j] = new JPanel();
-
-                if ((i + j) % 2 == 0) {
-                    squares[i][j].setBackground(Color.white);
-                } else {
-                    squares[i][j].setBackground(Color.black);
-                }
-            }        
-        
-            squares[0][0].add(new JLabel(new ImageIcon("C:/Users/Martin/Desktop/taarn.png")));//Tårn
-            squares[0][2].add(new JLabel(new ImageIcon("")));//løper
-
-            squares[0][4].add(new JLabel(new ImageIcon("")));//hest
-            squares[0][5].add(new JLabel(new ImageIcon("")));//løper
-            squares[0][7].add(new JLabel(new ImageIcon("")));//Taarn
-
-            squares[7][0].add(new JLabel(new ImageIcon("")));
-            squares[7][2].add(new JLabel(new ImageIcon("")));
-            squares[7][4].add(new JLabel(new ImageIcon("")));
-            squares[7][5].add(new JLabel(new ImageIcon("")));
-            squares[7][7].add(new JLabel(new ImageIcon("")));
-
+    private Brett brett;    
+    public Gui(String tittel) { 
+        setTitle(tittel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500,500));
+        setLayout(new BorderLayout());
+        add(new Rutenett(),BorderLayout.CENTER);
+        pack();
+    }
+    private class Rutenett extends JPanel{
+        private JPanel squares[][] = new JPanel[8][8];
+        public Rutenett(){
             for (int i = 0; i < 8; i++) {
-                squares[1][i].add(new JLabel(new ImageIcon("")));
-                squares[6][i].add(new JLabel(new ImageIcon("")));
+                for (int j = 0; j < 8; j++) {
+                    squares[i][j] = new JPanel();
+                    squares[i][j].setPreferredSize(new Dimension(50,50));
+                    add(squares[i][j]);
+                    if ((i + j) % 2 == 0) {
+                        squares[i][j].setBackground(new Color(160,82,45));
+                    } else {
+                        squares[i][j].setBackground(new Color(160,82,45,127));
+                    }
+                }        
             }
         }
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        
-        GUI b = new GUI();
-        
+        Gui b = new Gui("hore");
+        b.setVisible(true);        
     }
 }
