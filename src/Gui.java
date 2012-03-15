51e6113a42c163fcc8102a9b3255d51abd74d987
Martin <Martin@158.38.121.143>
2012-03-15 10:35:27 +0100

    marinB

--- src/Gui.java
+++ src/Gui.java
@@ -39,7 +39,7 @@ class GUI extends JFrame {
         }
         
         
-        squares[0][0].add(new JLabel(new ImageIcon("C:\Users\Martin\Desktop\taarn.png")));
+        squares[0][0].add(new JLabel(new ImageIcon("C:\Users\Martin\Desktop\taarn.png")));//Taarn
         squares[0][2].add(new JLabel(new ImageIcon("")));//løper
 
         squares[0][4].add(new JLabel(new ImageIcon("")));//hest
